72db7570792005c5b3d53287ab09a10914a0e7e7
STD\s20227075 <20227075-takahashikoei@hcs.ac.jp>
2024-12-06 09:47:19 +0900

    キャラクター選択画面[完成率50%]

--- Assets/Scripts/CharacterSelection/CharacterSelectionManager.cs
+++ Assets/Scripts/CharacterSelection/CharacterSelectionManager.cs
@@ -3,67 +3,139 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 using UnityEngine.UI;
+using System;
+
 public class CharacterSelectionManager : NetworkBehaviour
 {
-    // 現在選択しているキャラクター
-    private int _currentSelectionCharacter = 0;
+
+    // 現在選択しているキャラクター（共有する変数）
+    public static int CurrentSelectionCharacter { get; private set; } = 0;
 
     // 確定選択しているキャラクター
     private int _confirmedSelectionCharacter = default;
 
+    [SerializeField,Tooltip("キャラクターモデルを格納")]
+    private List<GameObject> _characterModel = new List<GameObject>();
+
+    private void Update()
+    {
+       
+    }
 
     //キャラクター1のボタンにつける
     public void OnClick1()
     {
-        _currentSelectionCharacter = 1;
+        DeleteCharacter();
+        CurrentSelectionCharacter = 1;
+
+        // リストの1番目のオブジェクトを取得して制御
+        GameObject secondObject = _characterModel[0];
+        secondObject.SetActive(true);
+        print(CurrentSelectionCharacter);
     }
 
     //キャラクター2のボタンにつける
     public void OnClick2()
     {
-        _currentSelectionCharacter = 2;
+        DeleteCharacter();
+        CurrentSelectionCharacter = 2;
+
+        // リストの2番目のオブジェクトを取得して制御
+        GameObject secondObject = _characterModel[1];
+        secondObject.SetActive(true);
+        print(CurrentSelectionCharacter);
     }
 
     //キャラクター3のボタンにつける
     public void OnClick3()
     {
-        _currentSelectionCharacter = 3;
+        DeleteCharacter();
+        CurrentSelectionCharacter = 3;
+
+        // リストの3番目のオブジェクトを取得して制御
+        GameObject secondObject = _characterModel[2];
+        secondObject.SetActive(true);
+        print(CurrentSelectionCharacter);
     }
 
     //キャラクター4のボタンにつける
     public void OnClick4()
     {
-        _currentSelectionCharacter = 4;
+        DeleteCharacter();
+        CurrentSelectionCharacter = 4;
+
+        // リストの4番目のオブジェクトを取得して制御
+        GameObject secondObject = _characterModel[3];
+        secondObject.SetActive(true);
+        print(CurrentSelectionCharacter);
     }
 
     //キャラクター5のボタンにつける
     public void OnClick5()
     {
-        _currentSelectionCharacter = 5;
+        DeleteCharacter();
+        CurrentSelectionCharacter = 5;
+
+        // リストの5番目のオブジェクトを取得して制御
+        GameObject secondObject = _characterModel[4];
+        secondObject.SetActive(true);
+        print(CurrentSelectionCharacter);
     }
 
     //キャラクター6のボタンにつける
     public void OnClick6()
     {
-        _currentSelectionCharacter = 6;
+        DeleteCharacter();
+        CurrentSelectionCharacter = 6;
+
+        // リストの6番目のオブジェクトを取得して制御
+        GameObject secondObject = _characterModel[5];
+        secondObject.SetActive(true);
+        print(CurrentSelectionCharacter);
     }
 
     //キャラクター7のボタンにつける
     public void OnClick7()
     {
-        _currentSelectionCharacter = 7;
+        DeleteCharacter();
+        CurrentSelectionCharacter = 7;
+
+        // リストの7番目のオブジェクトを取得して制御
+        GameObject secondObject = _characterModel[6];
+        secondObject.SetActive(true);
+        print(CurrentSelectionCharacter);
     }
 
     //キャラクター8のボタンにつける
     public void OnClick8()
     {
-        _currentSelectionCharacter = 8;
+        DeleteCharacter();
+        CurrentSelectionCharacter = 8;
+
+        // リストの8番目のオブジェクトを取得して制御
+        GameObject secondObject = _characterModel[7];
+        secondObject.SetActive(true);
+        print(CurrentSelectionCharacter);
     }
 
     // 選択しているキャラクターを確定する
     public void ConfirmedOnClick()
     {
-        _confirmedSelectionCharacter = _currentSelectionCharacter;
+        _confirmedSelectionCharacter = CurrentSelectionCharacter;
+        print(_confirmedSelectionCharacter);
+        print(CurrentSelectionCharacter);
+    }
+
+    /// <summary>
+    /// すべてのオブジェクトを非表示にする
+    /// </summary>
+    private void DeleteCharacter()
+    {
+
+        foreach (GameObject obj in _characterModel)
+        {
+            obj.SetActive(false);
+        }
     }
 
 }
