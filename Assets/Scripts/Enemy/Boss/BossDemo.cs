1b88a72b98efd6e62dcc310f104f87929806330d
STD\s20227075 <20227075-takahashikoei@hcs.ac.jp>
2024-12-13 14:59:03 +0900

    no message

---
+++ Assets/Scripts/Enemy/Boss/BossDemo.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using Fusion;
+using UniRx;
+
+public class BossDemo : BaseEnemy
+{
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
