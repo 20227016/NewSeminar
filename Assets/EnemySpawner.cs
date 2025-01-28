bd9a9e288a9f8b46b1a8c5f0a6fe948e78c8867c
STD\s20227075 <20227075-takahashikoei@hcs.ac.jp>
2025-01-28 12:26:19 +0900

    最新でーた　

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -3,17 +3,15 @@ using System.Collections.Generic;
 using Fusion;
 using Fusion.Sockets;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UniRx;
-using Cysharp.Threading.Tasks;
-using System.Collections;
 
 public class EnemySpawner : MonoBehaviour, INetworkRunnerCallbacks
 {
+    [SerializeField, Tooltip("ボスへ向かうテレポーター")]
+    private GameObject _bossTeleporter = default;
 
     [SerializeField, Tooltip("ネットワークランナープレハブ")]
     private NetworkRunner _networkRunnerPrefab = default;
-
 
     [SerializeField, Tooltip("エネミーのプレハブリスト")]
     private List<NetworkObject> _enemyPrefabs = new List<NetworkObject>();
@@ -21,6 +19,10 @@ public class EnemySpawner : MonoBehaviour, INetworkRunnerCallbacks
     [SerializeField, Tooltip("エネミーのスポーン位置リスト")]
     private List<Transform> _enemyStartPositions = new List<Transform>();
 
+
+    
+    private List<NetworkObject> _spawnedEnemies = new List<NetworkObject>();
+    private Subject<Unit> OnAllEnemiesDefeated = new Subject<Unit>();
 
     private void Start()
     {
@@ -30,21 +32,69 @@ public class EnemySpawner : MonoBehaviour, INetworkRunnerCallbacks
         Debug.Log(networkRunner);
 
         gameLauncher.StartGameSubject.Subscribe(_ => InitialSpawn(networkRunner));
+
+        // エネミー全滅時の通知を購読
+        OnAllEnemiesDefeated.Subscribe(_ => HandleAllEnemiesDefeated());
     }
 
-    // ComboCounterを生成するメソッド(スポナー専用)
+    /// <summary>
+    /// すべてのエネミーの状態(表示・非表示)を確認する 
+    /// </summary>
+    private void Update()
+    {
+        if (_spawnedEnemies.Count > 0 && AllEnemiesHidden())
+        {
+            OnAllEnemiesDefeated.OnNext(Unit.Default);
+        }
+    }
+
+    /// <summary>
+    /// エネミーの初期スポーン処理
+    /// </summary>
     private void InitialSpawn(NetworkRunner runner)
     {
         if (runner.IsServer)
         {
-
             for (int i = 0; i < _enemyPrefabs.Count; i++)
             {
-                
-                // ボスをスポーン
-                runner.Spawn(_enemyPrefabs[i], _enemyStartPositions[i].position, Quaternion.identity);
-                print($"エネミー {i} をスポーンしました");
+                // エネミーをスポーン
+                var spawnedEnemy = runner.Spawn(_enemyPrefabs[i], _enemyStartPositions[i].position, Quaternion.identity);
+                _spawnedEnemies.Add(spawnedEnemy);
+
+                // エネミーの非表示を監視
+                spawnedEnemy.gameObject.SetActive(true);
             }
+
+            Debug.Log($"{_spawnedEnemies.Count}体のエネミーをスポーンしました");
+        }
+    }
+
+    /// <summary>
+    /// 全てのエネミーが非表示かを確認
+    /// </summary>
+    /// <returns>全エネミーが非表示ならtrue</returns>
+    private bool AllEnemiesHidden()
+    {
+        foreach (var enemy in _spawnedEnemies)
+        {
+            if (enemy.gameObject.activeSelf)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// 全エネミーが非表示になったときの処理
+    /// </summary>
+    private void HandleAllEnemiesDefeated()
+    {
+        Debug.Log("全エネミーが倒されました！");
+        if (_bossTeleporter != null)
+        {
+            _bossTeleporter.SetActive(true);
         }
     }
 
