682eaca891241d8c101948252fb53a1f8a7949a0
STD\s20227075 <20227075-takahashikoei@hcs.ac.jp>
2025-01-31 10:02:49 +0900

    no message

--- Assets/WaveClear.cs
+++ Assets/WaveClear.cs
@@ -1,5 +1,8 @@
 using Fusion;
 using UnityEngine;
+using UniRx;
+using System.Linq;
+using System.Threading.Tasks;
 
 public class WaveClear : NetworkBehaviour
 {
@@ -10,6 +13,14 @@ public class WaveClear : NetworkBehaviour
     [Tooltip("ステージをわける仕切りEF")]
     private GameObject _wavePartitionEND = default;
 
+    [Tooltip("ボスへ向かうテレポーター")]
+    private GameObject _bossTeleporter = default;
+
+    /// <summary>
+    /// 敵管理クラス
+    /// </summary>
+    private EnemySpawner _enemySpawner = default;
+
 
     public override void Spawned()
     {
@@ -17,24 +28,15 @@ public class WaveClear : NetworkBehaviour
 
         _wavePartition = GameObject.Find("Gate");
         _wavePartitionEND = GameObject.Find("GateEND");
+        _bossTeleporter = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(obj => obj.name == "BosteReporter");
+        _enemySpawner = FindObjectOfType<EnemySpawner>();
 
-
-        //GameObject[] allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
-
-        //foreach (GameObject obj in allObjects)
-        //{
-        //    if (obj.name == "Gate")  // 1つ目
-        //    {
-        //        _wavePartition = obj;
-        //        print("取得成功: _wavePartition = " + _wavePartition.name);
-
-        //    }
-        //    else if (obj.name == "GateEND")  // 2つ目
-        //    {
-        //        _wavePartitionEND = obj;
-        //        print("取得成功: _wavePartitionEND = " + _wavePartitionEND.name);
-        //    }
-        //}
+        _enemySpawner.OnAllEnemiesDefeatedObservable.Subscribe(_ =>
+        {
+            // エネミー全滅時の処理を記述
+            Debug.Log("他のスクリプトでエネミー全滅イベントを受け取りました！");
+            HandleAllEnemiesDefeated();
+        }).AddTo(this);
 
         WaveGateOpen();
     }
@@ -43,22 +45,47 @@ public class WaveClear : NetworkBehaviour
     {
         print("WaveGateOpenが呼ばれました。RPCを起動します");
 
-        RPC_WaveGateOpen();
+        RPC_WaveGateOpen1();
     }
 
     [Rpc(RpcSources.All, RpcTargets.All)]
-    private void RPC_WaveGateOpen()
+    private void RPC_WaveGateOpen1()
     {
         if ((_wavePartition != null) && (_wavePartitionEND != null))
         {
             _wavePartitionEND.SetActive(false);
             _wavePartitionEND.SetActive(true);
-            _wavePartition.SetActive(false);
-            print("2Waveをクリアしました。ゲート開放！(ネットワーク)EnemySpawner1で実行しました");
+            WaveGateOpen2();
         }
         else
         {
             print("NULLです");
         }
+    }
+
+    private async void WaveGateOpen2()
+    {
+        await Task.Delay(1000);
+        RPC_WaveGateOpen3();
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.All)]
+    private void RPC_WaveGateOpen3()
+    {
+        _wavePartition.SetActive(false);
+        print("2Waveをクリアしました。ゲート開放！(ネットワーク)EnemySpawner1で実行しました");
+    }
+
+    private void HandleAllEnemiesDefeated()
+    {
+        print("テレポーター出現(ローカル)");
+        RPC_HandleAllEnemiesDefeated();
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.All)]
+    private void RPC_HandleAllEnemiesDefeated()
+    {
+        print("テレポーター出現(ネットワーク)");
+        _bossTeleporter.SetActive(true);
     }
 }
