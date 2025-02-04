40a86bb2dbb99152ff4e92b7841e21c256503037
STD\s20227016 <20227016-yumotoraiki@hcs.ac.jp>
2025-02-04 16:12:30 +0900

    プレイヤーのサウンド（80%完成）

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -1,5 +1,6 @@
 
 using UnityEngine;
+using Cysharp.Threading.Tasks;
 using Fusion;
 
 /// <summary>
@@ -7,7 +8,7 @@ using Fusion;
 /// 基本的に関数を呼び出し、その関数内でRPCが設定されているEF再生関数を呼び出し、全クライアントに通知する
 /// </summary>
 public class SoundManager : ISound
-{ 
+{
 
     /// <summary>
     /// 効果音を出力
@@ -15,7 +16,9 @@ public class SoundManager : ISound
     /// <param name="audioSource"></param>
     /// <param name="audioClip"></param>
     /// <param name="audioSpeed"></param>
-    public void ProduceSE(AudioSource audioSource, AudioClip audioClip, float audioSpeed , float audioVolume)
+    /// <param name="audioVolume"></param>
+    /// <param name="delay"></param>
+    public async void ProduceSE(AudioSource audioSource, AudioClip audioClip, float audioSpeed , float audioVolume , float delay)
     {
 
         if(audioSource == null || audioSource == null)
@@ -24,6 +27,7 @@ public class SoundManager : ISound
             return;
 
         }
+        await UniTask.Delay((int)(delay * 1000f));
         audioSource.pitch = audioSpeed;
         audioSource.volume = audioVolume;
         audioSource.PlayOneShot(audioClip);
