cb87b37390f84b0bd98d36c8ddac0e5f25a443f8
yamada-tomoya <20227038-yamadatomoya@hcs.ac.jp>
2025-02-07 12:23:21 +0900

    no message

--- Assets/Scripts/Scene/GameStart.cs
+++ Assets/Scripts/Scene/GameStart.cs
@@ -5,7 +5,7 @@ public class GameStart : MonoBehaviour
 {
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.anyKey)
         {
             SceneManager.LoadScene("高橋デバック");
         }
