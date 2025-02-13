14ede9e6463e63a7320b02cf771c8f2bcc9e1cfb
STD\s20227075 <20227075-takahashikoei@hcs.ac.jp>
2025-02-13 16:02:04 +0900

    no message

--- Assets/GameOverManagement.cs
+++ Assets/GameOverManagement.cs
@@ -13,6 +13,19 @@ public class GameOverManagement : NetworkBehaviour
 
     public override void Spawned()
     {
+        print("とりあえずゲームオーバーマネージャーは爆誕したお");
+        CharacterBase[] characters = FindObjectsOfType<CharacterBase>();
+
+        if (characters.Length > 0)
+        {
+            _characterBase = characters[0]; // ひとまず最初のキャラを取得
+            print($"取得完了！ {_characterBase.name}");
+
+        }
+        else
+        {
+            print("プレイヤーキャラクターが見つからない！やべえ！");
+        }
 
         // 死亡イベントの購読
         _characterBase.DeathSubject.Subscribe(_ =>
