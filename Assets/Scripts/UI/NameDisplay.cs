82f826280713f097fb59181376bcf44dd42a0477
yamada-tomoya <20227038-yamadatomoya@hcs.ac.jp>
2025-02-13 11:11:05 +0900

    Reapply "no message"
    This reverts commit d6d52e158f1aeb820953c8705417e4fc092b0376.

--- Assets/Scripts/UI/NameDisplay.cs
+++ Assets/Scripts/UI/NameDisplay.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerNameDisplay : MonoBehaviour
+public class NameDisplay : MonoBehaviour
 {
     private void Update()
     {
