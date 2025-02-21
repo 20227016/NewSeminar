57dbd7e684ec8385c8a3435ebc34d80ec21db5ba
STD\s20227016 <20227016-yumotoraiki@hcs.ac.jp>
2025-02-21 13:15:28 +0900

    no message

--- Assets/Scripts/Enemy/Boss/LightningChain.cs
+++ Assets/Scripts/Enemy/Boss/LightningChain.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class LightningChain : BaseEnemy
 {
 
-    [SerializeField,Header("レーザーの攻撃範囲の見た目")]
-    private GameObject _laserRangeView = default;
+    [SerializeField, Header("消える時間")]
+    private float _time = default;
 
-    [SerializeField, Header("レーザーの見た目（エフェクト）")]
-    private GameObject _laserView = default;
+    private float _timer = default;
 
     protected override void OnDeath()
     {
@@ -22,14 +21,54 @@ public class LightningChain : BaseEnemy
     void Start()
     {
 
+      
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
 
+        _timer += Time.deltaTime;
+        if (_time <= _timer)
+        {
+
+            _timer = 0;
+            EnemyDespawn();
+
+        }
 
     }
+
+    /// <summary>
+    /// ダメージを与える処理
+    /// </summary>
+    /// <param name="other"></param>
+    public override void OnTriggerEnter(Collider hitCollider)
+    {
+       
+    }
+
+    /// <summary>
+    /// ダメージを与える処理
+    /// </summary>
+    /// <param name="other"></param>
+    public void OnTriggerStay(Collider hitCollider)
+    {
+
+        if (!hitCollider.CompareTag("Player"))
+        {
+            return;
+        }
+
+        IReceiveDamage receiveDamage = hitCollider.GetComponent<IReceiveDamage>();
+        if (receiveDamage == null)
+        {
+            return;
+        }
+        // 攻撃力に攻撃倍率を渡して渡す
+        receiveDamage.ReceiveDamage((int)(_enemyStatusStruct._attackPower * _currentAttackMultiplier));
+
+    }
+
 }
